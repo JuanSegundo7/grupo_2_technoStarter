b77fba2e3e63455f8504b5eef7a6fc59062334ed
JuanSegundo7 <juansepincha@gmail.com>
2021-05-24 18:27:17 -0300

    actualizar vistas del proyecto

--- website/app.js
+++ website/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "./public")));
-app.listen(3000, () => console.log("Server star in http://localhost:3000"));
+app.listen(3000, () => console.log("Server start in http://localhost:3000"));
 
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "./views", "home.html"))
