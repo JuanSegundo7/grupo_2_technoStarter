6f48e7a79b5fe8ca560980214889f04b58f6a383
JuanSegundo7 <juansepincha@gmail.com>
2021-06-22 20:09:58 -0300

    agregando controladores

--- website/src/controllers/mainController.js
+++ website/src/controllers/mainController.js
@@ -1,12 +1,19 @@
 const main = {
     index: (req,res) => {
-        return res.render("home")
+        return res.render("home");
     },
 
     nosotros: (req,res) => {
-        return res.render("nosotros")
+        return res.render("nosotros");
     },
 
-};
+    crear: (req,res) => {
+        return res.render("crearProducto");
+    },
+
+    editar: (req,res) => { 
+        return res.render("editarProducto");
+    }
+}
 
 module.exports = main;
