68358891728bc0007aaddccb28bc25cb3cb796af
JuanSegundo7 <juansepincha@gmail.com>
2021-07-01 21:17:31 -0300

    se reune el team again

--- website/src/models/proyecto.js
+++ website/src/models/proyecto.js
@@ -1,16 +1,71 @@
 const path = require("path");
 const fs = require("fs");
 const model = {
-    allProyecto: function (){
-        const directory = path.resolve(__dirname, "../data", "proyectos.json");
-        const file = fs.readFileSync(directory, "utf-8"/*preguntarle a edu*/);
+    allProyect: function() {
+        const directory = path.resolve(__dirname,"../data","proyectos.json");
+        const file = fs.readFileSync(directory,"utf-8");
         const convert = JSON.parse(file);
         return convert;
     },
-    oneProyecto: function (id) {
-        const proyectos = this.allProyecto();
+    oneProyect: function (id) {
+        const proyectos = this.allProyect();
         let resultado = proyectos.find(element => element.id == id);
         return resultado;
+    },
+    newProyect: function(data,file){
+        const directory = path.resolve(__dirname, "../data", "proyectos.json");
+        let productos = this.allProyect();
+        let nuevo = {
+            id: productos.length > 0 ? productos[productos.length-1].id + 1 : 1,
+            nombre: data.nombre,
+            contribucionActual: 0,
+            contribucionFinal: 10000,
+            fecha: data.fecha,
+            patrocinadores: 20,
+            ubicacion: data.ubicacion,
+            autor: 1,
+            categorias: parseInt(data.categorias),
+            images: file.filename != undefined ? file.filename : "defaultImagen.jpg",
+        }
+        productos.push(nuevo);
+        fs.writeFileSync(directory, JSON.stringify(productos,null,2));
+        return true
+    },
+    proyectsByCategory: function(idCategory){
+        const proyectos = this.allProyect();
+        let resultado = proyectos.filter(element => element.category == idCategory);
+        return resultado;
+    },
+    editProyect: function(data,file,id){
+        const directory = path.resolve(__dirname,"../data","proyectos.json")
+        let proyectos = this.allProyect();
+        proyectos.map(proyecto => {
+            if(proyecto.id == id){
+                proyecto.nombre = data.nombre,
+                proyecto.contribucionFinal = 10000,
+                proyecto.fecha = data.fecha,
+                proyecto.patrocinadores = 20,
+                proyecto.ubicacion = data.ubicacion,
+                proyecto.autor = 1,
+                proyecto.categoria = parseInt(data.categorias),
+                proyecto.images = file.filename
+                return proyecto
+            }
+            return proyecto
+        })
+        fs.writeFileSync(directory, JSON.stringify(directory,null,2));
+        return true;
+    },
+    deleteProyect: function (id) {
+        const directory = path.resolve(__dirname,"../data","proyectos.json")
+        let proyectos = this.allProyect();
+        let deleted = this.oneProyect(id);
+        // eliminamos la imagen de la carpeta upload
+        fs.unlinkSync(path.resolve(__dirname, "../../public/uploads/products", deleted.image))
+        // filtarmos el producto que deaseamos eliminar
+        proyectos = proyectos.filter(proyectos => proyectos.id != deleted.id )
+        fs.writeFileSync(directory,JSON.stringify(proyectos,null,2));
+        return true;
     }
 }
 
--- website/src/routes/productRouter.js
+++ website/src/routes/productRouter.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const productController = require("../controllers/productController");
+const multer = require('multer');
+let dest = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let extension = path.extname(file.originalname);
+        if(extension.indexOf("jpg") > 0){
+            cb(null, path.resolve(__dirname,"../../public/uploads","products"))
+        }
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+})
+const upload = multer({storage:dest});
 
 router.get("/producto", productController.index);
-router.get("/crear-producto", productController.crear);
-router.get("/editar-producto", productController.editar);
+router.get("/crear-producto", productController.create);
+router.get("/editar-producto", productController.edit);
+// router.get("/:id",productController)
 
 module.exports = router;
