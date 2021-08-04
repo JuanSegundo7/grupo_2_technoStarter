1d0ce4e5a90e11d7523719a87cb26a061d9b8c02
JuanSegundo7 <juansepincha@gmail.com>
2021-08-04 20:08:56 -0300

    agregando funcionalidad ejs a cart

--- website/src/controllers/cartController.js
+++ website/src/controllers/cartController.js
@@ -1,7 +1,11 @@
+// ************ Require's ************
+
+const proyecto = require("../models/proyecto");
+
+// ************ Controller ************
+
 const cart = {
-    cart: (req, res) => {
-        return res.render("checkout/cart");
-    },
+    cart: (req, res) => {return res.render("checkout/cart", {proyectos:proyecto.random()});},
 };
 
 module.exports = cart;
