09efe6a1c4d56998909d4e91c794c741607b5514
Guido-Padin <guidopadin17@gmail.com>
2021-08-12 19:01:33 -0300

    archivos sql incompletos

--- website/TechnoStarter.sql
+++ website/TechnoStarter.sql
@@ -4,44 +4,46 @@ CREATE DATABASE TechnoStarter;
 USE TechnoStarter;
 
 CREATE TABLE categorias(
-    id TINYINT(3) NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(15) NOT NULL,
-    PRIMARY KEY(id)
+  id TINYINT(3) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(15) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE usuarios (
-    id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(15) NOT NULL,
-    apellido VARCHAR(30) NOT NULL,
-    email VARCHAR(80) NOT NULL,
-    clave VARCHAR(200) NOT NULL,
-    ubicacion VARCHAR(80) NOT NULL,
-    avatar VARCHAR(80) NOT NULL,
-    PRIMARY KEY(id)
+  id INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(15) NOT NULL,
+  apellido VARCHAR(30) NOT NULL,
+  email VARCHAR(80) NOT NULL,
+  clave VARCHAR(200) NOT NULL,
+  ubicacion VARCHAR(80) NOT NULL,
+  avatar VARCHAR(80) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE tipo_contribucion (
-    id TINYINT(4) NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(6) NOT NULL,
-    monto FLOAT unsigned NOT NULL,
-    PRIMARY KEY(id)
-);
+  id TINYINT(4) NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(6) NOT NULL,
+  monto FLOAT unsigned NOT NULL,
+  PRIMARY KEY(id)
+  );
 
 CREATE TABLE proyectos (
-    id INT NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
-    contribucion_actual INT unsigned NOT NULL DEFAULT 0,
-    contribucion_final INT unsigned NOT NULL,
-    fecha_inicial DATE NOT NULL,
-    fecha_limite DATE NOT NULL,
-    usuario_id INT NOT NULL,
-    categoria_id TINYINT(3) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (categoria_id) REFERENCES categorias (id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
-);
-
-CREATE TABLE contribucion_usuarios (
+  id INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(100) NOT NULL,
+  contribucion_actual FLOAT unsigned NOT NULL DEFAULT 0,
+  contribucion_final FLOAT unsigned NOT NULL,
+  texto TEXT NOT NULL,
+  fecha_inicial DATE NOT NULL,
+  fecha_limite DATE NOT NULL,
+  usuario_id INT NOT NULL,
+  categoria_id TINYINT(3) NOT NULL,
+  imagenes TEXT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (categoria_id) REFERENCES categorias (id),
+  FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
+  );
+  
+  CREATE TABLE contribucion_usuarios (
     id INT NOT NULL AUTO_INCREMENT,
     usuario_id INT NOT NULL,
     proyecto_id INT NOT NULL,
@@ -50,4 +52,4 @@ CREATE TABLE contribucion_usuarios (
     FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
     FOREIGN KEY (proyecto_id) REFERENCES proyectos (id),
     FOREIGN KEY (contribucion_id) REFERENCES tipo_contribucion (id)
-);
+    );
