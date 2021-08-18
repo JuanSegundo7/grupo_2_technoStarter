07254ee3b6cc66425171e43c8671ec98f76992f7
JuanSegundo7 <juansepincha@gmail.com>
2021-08-18 18:53:35 -0300

    actualizando tablas

--- website/src/sql/TechnoStarter.sql
+++ website/src/sql/TechnoStarter.sql
@@ -37,19 +37,28 @@ CREATE TABLE proyectos (
   fecha_limite DATE NOT NULL,
   usuario_id INT NOT NULL,
   categoria_id TINYINT(3) NOT NULL,
-  imagenes TEXT NOT NULL,
+  imagenes_id INT NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY (categoria_id) REFERENCES categorias (id),
-  FOREIGN KEY (usuario_id) REFERENCES usuarios (id)
+  FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
+  FOREIGN KEY (imagenes_id) REFERENCES imagenes (id)
 );
 
+CREATE TABLE imagenes (
+  id INT NOT NULL AUTO_INCREMENT,
+  url_imagen VARCHAR(255) NOT NULL,
+  proyecto_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (proyecto_id) REFERENCES proyectos (id)
+)
+
 CREATE TABLE contribucion_usuarios (
-    id INT NOT NULL AUTO_INCREMENT,
-    usuario_id INT NOT NULL,
-    proyecto_id INT NOT NULL,
-    contribucion_id TINYINT(4) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
-    FOREIGN KEY (proyecto_id) REFERENCES proyectos (id),
-    FOREIGN KEY (contribucion_id) REFERENCES tipo_contribucion (id)
+  id INT NOT NULL AUTO_INCREMENT,
+  usuario_id INT NOT NULL,
+  proyecto_id INT NOT NULL,
+  contribucion_id TINYINT(4) NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (usuario_id) REFERENCES usuarios (id),
+  FOREIGN KEY (proyecto_id) REFERENCES proyectos (id),
+  FOREIGN KEY (contribucion_id) REFERENCES tipo_contribucion (id)
 );
